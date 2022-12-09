97eac10a77fc05b874eed33b1c932dac0fecb171
AnneMizette <112874930+AnneMizette@users.noreply.github.com>
2022-12-09 12:31:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,33 @@ main_keyboard = [
 REMINDER_TITLE, REMINDER_DATE, REMINDER_TIME, REMINDER_INFO = range(4)
 DELETE_REMINDER_INDEX = range(1)
 ADD_UTC = range(1)
+
+def is_new_user(user, username, first_name, last_name):
+    user = str(user)
+    with open("data.json", "r+") as file:
+        content = json.load(file)
+        if user not in content["users"]:
+            content["users"][user] = {
+                "username": username,
+                "first name": first_name,
+                "last name": last_name,
+                "utc": "",
+                "reminders": [],
+            }
+            file.seek(0)
+            json.dump(content, file)
+            file.truncate()
+
+            return True
+        else:
+            return False
+
+
+def json_add_utc(user, utc):
+    user = str(user)
+    with open("data.json", "r+") as file:
+        content = json.load(file)
+        content["users"][user]["utc"] = utc
+        file.seek(0)
+        json.dump(content, file)
+        file.truncate()
