f495431803f920332ed4dfac3e0bc69d80dd33fe
Dan-Austin <112837126+Dan-Austin@users.noreply.github.com>
2022-12-09 12:44:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,3 +58,93 @@ def json_add_utc(user, utc):
         file.seek(0)
         json.dump(content, file)
         file.truncate()
+
+def json_get_utc(user):
+    user = str(user)
+    with open("data.json", "r") as file:
+        content = json.load(file)
+        return content["users"][user]["utc"]
+
+
+def json_get_info(user):
+    user = str(user)
+    with open("data.json", "r") as file:
+        content = json.load(file)
+        return content["users"][user]["reminders"][0]
+
+
+def json_add_reminder(user):
+    user = str(user)
+    with open("data.json", "r+") as file:
+        content = json.load(file)
+        content["users"][user]["reminders"].insert(
+            0, {"title": "", "date": "", "time": "", "info": ""}
+        )
+        file.seek(0)
+        json.dump(content, file)
+        file.truncate()
+
+
+def json_add_reminder_info(user, key, value):
+    user = str(user)
+    key = str(key)
+    with open("data.json", "r+") as file:
+        content = json.load(file)
+        content["users"][user]["reminders"][0][key] = value
+        file.seek(0)
+        json.dump(content, file)
+        file.truncate()
+
+
+def json_cancel_add_reminder_process(user):
+    user = str(user)
+    with open("data.json", "r+") as file:
+        content = json.load(file)
+        del content["users"][user]["reminders"][0]
+        file.seek(0)
+        json.dump(content, file)
+        file.truncate()
+
+
+def json_delete_reminder(user, index):
+    user = str(user)
+    index = index - 1
+
+    with open("data.json", "r+") as file:
+        content = json.load(file)
+
+        del content["users"][user]["reminders"][index]
+        file.seek(0)
+        json.dump(content, file)
+        file.truncate()
+
+
+def json_get_reminders_list(user):
+    user = str(user)
+    with open("data.json", "r+") as file:
+        content = json.load(file)
+        content = content["users"][user]["reminders"]
+
+        if len(content) == 0:
+            message = "📪 You have no reminders!"
+            return message
+
+        else:
+            message = ""
+            for i in range(len(content)):
+                title = content[i]["title"]
+                date = content[i]["date"]
+                time = content[i]["time"]
+                info = content[i]["info"]
+
+                message += f"""
+*Reminder {i+1}*
+------------------
+_Title_ : {title}
+_Date_ : {date}
+_Time_ : {time}
+_Info_ : {info}
+"""
+            return message
+
+        
